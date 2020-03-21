dec7ead6c7b03a4fe49b28d1c6835bc12bf5413f
alexispages <55453499+alexispages@users.noreply.github.com>
2020-03-21 18:27:01 +0100

    Version Finale du script
    - Suppression de la direction du vent
    - Redirection du texte généré vers le fichier weather.txt

--- weather.sh
+++ weather.sh
@@ -2,8 +2,7 @@
 
 temp=$(weather lfmu | awk 'NR==3{sub(/\(/, ""); print $4}')
 humidity=$(weather lfmu | awk 'NR == 4{print $3}')
-wind_dir=$(weather lfmu | awk 'NR==5{print $4}')
 wind_speed_mph=$(weather lfmu | awk 'NR==5{print $8}')
 wind_speed_kmh=$(echo "$wind_speed_mph*1.60934" | bc | awk '{printf("%d\n",$1 + 0.5)}')
 
-echo "Aujourd'hui, la température est de $temp degrés avec une humidité de $humidity. Le vent viendra du $wind_dir avec une vitesse de $wind_speed_kmh km/h."
+echo "Aujourd'hui, la température est de $temp degrés avec une humidité de $humidity. Le vent viendra du $wind_dir avec une vitesse de $wind_speed_kmh km/h." > weather.txt
