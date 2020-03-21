d067cf50241eae8d98d2e14d53922ea81ec9a29e
alexispages <55453499+alexispages@users.noreply.github.com>
2020-03-21 18:29:59 +0100

    Commentaires Script

--- weather.sh
+++ weather.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+# Script permettant d'obtenir un fichier txt à partir du bulletin météo de la commande weather
 
 temp=$(weather lfmu | awk 'NR==3{sub(/\(/, ""); print $4}')
 humidity=$(weather lfmu | awk 'NR == 4{print $3}')
 wind_speed_mph=$(weather lfmu | awk 'NR==5{print $8}')
+
+# Conversion MPH en KM/H + arrondi à l'unité
 wind_speed_kmh=$(echo "$wind_speed_mph*1.60934" | bc | awk '{printf("%d\n",$1 + 0.5)}')
 
 echo "Aujourd'hui, la température est de $temp degrés avec une humidité de $humidity. Le vent viendra du $wind_dir avec une vitesse de $wind_speed_kmh km/h." > weather.txt
