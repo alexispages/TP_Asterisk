8ac873f76c206d3b60fe863ff2569f7094f2c321
alexispages <55453499+alexispages@users.noreply.github.com>
2020-03-22 16:16:40 +0100

    Adaptation texte généré

--- weather.sh
+++ weather.sh
@@ -8,4 +8,4 @@ wind_speed_mph=$(weather lfmu | awk 'NR==5{print $8}')
 # Conversion MPH en KM/H + arrondi à l'unité
 wind_speed_kmh=$(echo "$wind_speed_mph*1.60934" | bc | awk '{printf("%d\n",$1 + 0.5)}')
 
-echo "Aujourd'hui, la température est de $temp degrés avec une humidité de $humidity. Le vent viendra du $wind_dir avec une vitesse de $wind_speed_kmh km/h." > weather.txt
+echo "Aujourd'hui, la température est de $temp degrés avec une humidité de $humidity. Le vent souffle a une vitesse de $wind_speed_kmh kilomètres par heure." > weather.txt
